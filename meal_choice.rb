b9949adc6e351b72c46b79129cb8ca52c13af22e
Alyssa Yesilyurt <alyssa.yesilyurt@gmail.com>
2019-05-23 16:43:37 +0000

    Automatically backed up by Learn

--- meal_choice.rb
+++ meal_choice.rb
@@ -1,4 +1,6 @@
 # Define a method meal_choice that returns the meal_choice that was
 # passed into it and defaults to meat.
-def meal_choice(type="meat", *p)
+def meal_choice(meal = "meat")
+
+end
 
